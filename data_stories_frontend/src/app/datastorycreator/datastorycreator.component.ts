1a6900f80d2463f4d271288cad6c8ab1add4873b
juliansaar <saarjulian@gmx.de>
2022-10-06 08:51:32 +0200

    erstes Fornular zur Erstellung DataStories

--- data_stories_frontend/src/app/datastorycreator/datastorycreator.component.ts
+++ data_stories_frontend/src/app/datastorycreator/datastorycreator.component.ts
@@ -8,12 +8,20 @@ import { ApiClientService } from '../service/api-client.service';
 export class DatastorycreatorComponent implements OnInit {
   name : string;
   length : number;
+  fragerunde: number;
+  adressat: string;
+  stand: string;
+  datensatz: string;
+  zeitraum_von: Date;
+  zeitraum_bis: Date;
+  messungsintervall: string;
+  eintraege: number;
   constructor(private apiclient: ApiClientService) { }
 
   ngOnInit(): void {
   }
 
   click(){
-    this.apiclient.createDataStory(this.name, this.length);
+    this.apiclient.createDataStory(this.name, this.length, this.fragerunde, this.adressat,this.stand,this.datensatz,this.zeitraum_bis,this.zeitraum_von,this.messungsintervall,this.eintraege);
   }
 }
