86a7f00bb3abb4d9e06d8b6f3dfd04c93dc7ad73
juliansaar <saarjulian@gmx.de>
2022-12-10 18:19:57 +0100

    survey fix

--- data_stories_frontend/src/app/survey/survey.component.ts
+++ data_stories_frontend/src/app/survey/survey.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
-import { display } from 'html2canvas/dist/types/css/property-descriptors/display';
+import { Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-survey',
@@ -7,48 +7,15 @@ import { display } from 'html2canvas/dist/types/css/property-descriptors/display
   styleUrls: ['./survey.component.css']
 })
 export class SurveyComponent implements OnInit {
-  @ViewChild('text') text: ElementRef;
-  @ViewChild('nein') nein: ElementRef;
-  show = false;
+  show: boolean;
+  show2 : boolean;
+  show3: boolean;
+  show4 : boolean;
   constructor() { }
 
  ngOnInit(): void {
   
-let nein = document.getElementById('question1no');
-let textfield = document.getElementById('text');
-
-console.log(nein)
-console.log(textfield)
-
-if(nein){
-  console.log("in If")
-  this.show=true;
-}else{
-  this.show=false;
-}
-
  }
-   /* if(this.nein.nativeElement.checked){
-      console.log('in IF');
-      this.show = true;
-     }
-
-  }*/
-
-  /*ngAfterViewInit(): void{
-    this.text.nativeElement.innerHTML = "in AfterViewInit";
-    console.log(this.nein.nativeElement.checked)
-
-   if(this.nein.nativeElement.checked === true){
-    this.text.nativeElement.innerHTML = "in IF"
-    this.text.nativeElement.hidden == true;
-   }
-    
-  }
-
-  ngOnChanges(changes: SimpleChanges){
-    console.log(changes);
-  }*/
 
 }
 
